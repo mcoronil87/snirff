67c52ec5bfcec0a6a59ce9360b3cfe724b99d30f
mcoronil87 <mcoronil87@gmail.com>
2026-08-26 12:05:17 +0200

    Delete src directory

--- src/data.js
+++
@@ -1,7 +0,0 @@
-export const FILTER_CATS = [
-  { id: 'all', label: 'Todos' },
-  { id: 'pip', label: 'Pipicanes' },
-  { id: 'park', label: 'Parques' },
-  { id: 'grass', label: 'Hierba' },
-  { id: 'cafe', label: 'Cafeterías' },
-];
